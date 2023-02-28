7d50d8803b6c3912246c850c81c242c3ed9bc155
maxgamezwtf <75334970+maxgamezwtf@users.noreply.github.com>
2023-02-28 08:50:10 -0600

    Solamente hace falta Timer

--- ImageTarget-ArcheryTarget/Assets/Scripts/Explode.cs
+++ ImageTarget-ArcheryTarget/Assets/Scripts/Explode.cs
@@ -2,25 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class explode : MonoBehaviour{
-    public GameObject explosion;
+public class Explode : MonoBehaviour{
+	public GameObject explosion;
+    public GameObject scoreToSpawn;
     public GameObject enemyToSpawn;
     Vector3 killPos;
     Quaternion killRot;
     public float waitTime = 0.5f;
+    bool bulletCollission = false; // to avoid hittimg multiple dpiders with same bullet
 
-    private void OnCollisionEnter(Collision collision){
-        if(collision.transform.tag == "Target"){
-            Destroy(collision.transform.gameObject);
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.transform.tag == "Spider" && bulletCollission == false)
+        {
+            Destroy(collision.transform.gameObject); // destroy spider
+            Scoring.score += 5;
+
+            bulletCollission = true;
+
             killPos = collision.transform.position;
             killRot = collision.transform.rotation;
             StartCoroutine(SpawnEnemyAgain());
-            Instantiate(explosion, collision.transform.position, collision.transform.rotation); 
+            Destroy(Instantiate(explosion, collision.transform.position, collision.transform.rotation),waitTime);
+            Destroy(Instantiate(scoreToSpawn, collision.transform.position, collision.transform.rotation), waitTime);
         }
     }
-    IEnumerator SpawnEnemyAgain(){
+    IEnumerator SpawnEnemyAgain()
+    {
         yield return new WaitForSeconds(waitTime);
         Instantiate(enemyToSpawn, killPos, killRot);
+        bulletCollission = false;
+        Destroy(gameObject); // destroy bullet
 
     }
 }
