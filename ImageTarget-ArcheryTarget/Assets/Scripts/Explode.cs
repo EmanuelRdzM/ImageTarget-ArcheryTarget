4734d630dc5c4668215d0b40c930655d5addadf4
Emanuel-Rdz <j.emanuel.rdz.m@hotmail.com>
2023-02-28 06:39:07 -0600

    Parte4
    Se añadio los nuevos modelos del castor y martillo,
    re realizo el cambio de modelos, creación de prebas,
    se añadio ajuste de camara AR al session con vuforiía en los componentes de la misma camara

--- ImageTarget-ArcheryTarget/Assets/Scripts/Explode.cs
+++ ImageTarget-ArcheryTarget/Assets/Scripts/Explode.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class explode : MonoBehaviour{
-    public GameObject explosion;
+public class Explode : MonoBehaviour{
+	public GameObject explosion;
     public GameObject enemyToSpawn;
     Vector3 killPos;
     Quaternion killRot;
@@ -23,4 +23,5 @@ public class explode : MonoBehaviour{
         Instantiate(enemyToSpawn, killPos, killRot);
 
     }
+
 }
